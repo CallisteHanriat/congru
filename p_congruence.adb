857276cf8c29fb89485e7467304c79c3c698b0f8
couim <couim38@gmail.com>
2014-10-30 23:49:59 +0100

    ajout du fichier p_congruence.adb et ajout de specifications dans le code du fichier p_congruence.ads

---
+++ p_congruence.adb
@@ -0,0 +1,6 @@
+With ada.Text_IO; 
+Use ada.Text_IO;
+
+package body p_congruence is
+
+end p_congruence;
--- p_congruence.ads
+++ p_congruence.ads
@@ -10,5 +10,13 @@ type tr_couple is record
 	modulo : integer;
 end record;
 
+function traitement1 (couple: in tr_couple) return integer;
+--traite le couple, pour retourner congru.
+
+function congruence (val : in integer; modulo: in integer) return integer;
+--traite une val, pour ressortir la congruence [modulo].
+
+function fermatpossible (couple : in tr_couple) return boolean;
+--traite un couple et retourne vrai ou faux si on peut utiliser fermat. 
 
 end p_congruence;
