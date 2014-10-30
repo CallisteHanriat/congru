04801c5d0f1df50bca0353b633ac230591ee6c64
couim <couim38@gmail.com>
2014-10-30 23:32:16 +0100

    declaration du type tr_couple dans le .ads et ajout de description dans le schema_congru.odp

--- p_congruence.ads
+++ p_congruence.ads
@@ -1,5 +1,14 @@
 with ada.text_io ;
 use ada.text_io ;
 
---des choses importantes
--- ajoute des choses encore.
+package p_congruence.ads is
+--type declaration tr_couple. 
+type tr_couple is record
+	value : integer;
+	puiss : integer;
+	congru : integer;
+	modulo : integer;
+end record;
+
+
+end p_congruence;
