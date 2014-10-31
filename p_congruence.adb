449ae88181b57ec9ad5daa39c1862eca8fc72810
couim <couim38@gmail.com>
2014-11-01 00:04:50 +0100

    debut des prototypes pour les fonctions dans le p_congruence.adb. Aucun corps n'a été ecrit encore. juste les procédures

--- p_congruence.adb
+++ p_congruence.adb
@@ -3,4 +3,21 @@ Use ada.Text_IO;
 
 package body p_congruence is
 
+function traitement1 (couple : in tr_couple) return integer is
+--traite le couple, pour retourner congru.
+begin
+	
+end traitement1;
+----------------------------
+function congruence (val : in integer; modulo: in integer) return integer is
+--traite une val, pour ressortir la congruence [modulo].
+begin
+	
+end congruence;
+----------------------------
+function fermatpossible(couple:in tr_couple) return boolean is
+--traite un couple et retourne vrai ou faux si on peut utiliser fermat. 
+begin
+
+end fermatpossible;
 end p_congruence;
